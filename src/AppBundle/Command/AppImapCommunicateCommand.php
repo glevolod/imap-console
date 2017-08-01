fea48dbf3cfac36c4315cfb0def9f9147e9a910e
Vladimir <g86vladimir@gmail.com>
2017-08-02 02:49:47 +0300

    infinite input

--- src/AppBundle/Command/AppImapCommunicateCommand.php
+++ src/AppBundle/Command/AppImapCommunicateCommand.php
@@ -22,33 +22,62 @@ class AppImapCommunicateCommand extends ContainerAwareCommand
 
 //        $username = readline('Username: ');
 //        $password = readline('Password: ');
-
         $username = 'g86vladimir@gmail.com';
-        $password = 'V_G862356q';
-
+        $password = 'V_G862356qs';
 
         /** @var ImapCommunicator $imapCommunicator */
         $imapCommunicator = $this->getContainer()->get('app.imap_communicator');
 
-        $imapCommunicator->open($username, $password);
-
-        $folders = $imapCommunicator->getFoldersNames();
-
-        foreach ($folders as $key => $name) {
-            $output->writeln($key . " " . $name);
+        if (!$imapCommunicator->open($username, $password)) {
+            $output->writeln($imapCommunicator->getLastErrorMessage());
+            return;
         }
+return;
 
+        $previousCommand = 'start';
 
-        $mails = $imapCommunicator->getFolderMailsList();
-
-        foreach ($mails as $key => $mail) {
-            $output->writeln($key . "\t" . $mail['from'] . "\t" . $mail['subject'] . "\t" . $mail['date']);
-        }
-
-        $mailBody = $imapCommunicator->getMailBody($mail['uid']);
-        $output->writeln($mailBody);
-
-        $output->writeln('Finished.');
+        $folders = [];
+        $mails = [];
+        $invitationText = 'Enter command: ';
+        do {
+            $userInput = readline($invitationText);
+            if (empty($userInput)) {
+                continue;
+            }
+            switch ($userInput) {
+                case 'get_folders':
+                    $previousCommand = 'get_folders';
+                    $invitationText = 'Enter command or folder number: ';
+                    $folders = $imapCommunicator->getFoldersNames();
+                    foreach ($folders as $key => $folder) {
+                        $output->writeln($key . " " . $folder['prettyName']);
+                    }
+                    break;
+                case ($previousCommand == 'get_folders' && in_array($userInput, array_keys($folders))):
+                    $previousCommand = 'folder_by_number';
+                    $invitationText = 'Enter command or mail number: ';
+                    $mails = $imapCommunicator->getFolderMailsList($folders[$userInput]['originalName']);
+                    foreach ($mails as $key => $mail) {
+                        $output->writeln($key . "\n" . $mail['from'] . "\n\t" . $mail['subject'] . "\n\t" . $mail['date']);
+                    }
+                    break;
+                case (($previousCommand == 'folder_by_number' || $previousCommand == 'mail_by_number') && in_array($userInput, array_keys($mails))):
+                    $previousCommand = 'mail_by_number';
+                    $invitationText = 'Enter command or mail number: ';
+                    $mailBody = $imapCommunicator->getMailBody($mails[$userInput]['uid']);
+                    $output->writeln($mailBody);
+                    break;
+                case (mb_strtolower($userInput) == 'exit' || mb_strtolower($userInput) == 'quit'):
+                    $output->writeln('Bye!');
+                    return;
+                    break;
+                case (mb_strtolower($userInput) == 'help'):
+                    $output->writeln('Available commands: get_folders, quit, exit, help');
+                    break;
+                default:
+                    $output->writeln("Invalid command. Try to enter 'help'");
+                    break;
+            }
+        } while (true);
     }
-
 }
--- src/AppBundle/Service/ImapCommunicator.php
+++ src/AppBundle/Service/ImapCommunicator.php
@@ -12,12 +12,19 @@ class ImapCommunicator
     private $username;
     private $password;
 
+    private $error;
+
 
     public function open($username, $password)
     {
         $this->username = $username;
         $this->password = $password;
-        $this->imap = imap_open(self::MAIL_BOX_PATH, $username, $password);
+        try {
+            $this->imap = imap_open(self::MAIL_BOX_PATH, $username, $password);
+        } catch (\Throwable $exception) {
+            $this->error = $exception;
+            return false;
+        }
         return true;
     }
 
@@ -27,8 +34,9 @@ class ImapCommunicator
         $folderNumber = 1;
 
         foreach ($folders as $folder) {
-            $folder = str_replace(self::MAIL_BOX_PATH, "", mb_convert_encoding($folder, "UTF-8", "UTF7-IMAP"));
-            $foldersArr[$folderNumber] = $folder;
+            $folderOriginalName = str_replace(self::MAIL_BOX_PATH, "", $folder);
+            $folderPrettyName = str_replace(self::MAIL_BOX_PATH, "", mb_convert_encoding($folder, "UTF-8", "UTF7-IMAP"));
+            $foldersArr[$folderNumber] = ['originalName' => $folderOriginalName, 'prettyName' => $folderPrettyName];
             $folderNumber++;
         }
         return $foldersArr;
@@ -40,7 +48,7 @@ class ImapCommunicator
         $numMessages = imap_num_msg($imap);
         $num = 1;
         $messageDetails = [];
-        for ($i = $numMessages; $i > ($numMessages - self::NUM_MESSAGES); $i--) {
+        for ($i = $numMessages; $i > ($numMessages - self::NUM_MESSAGES) && $i != 0; $i--) {
             $header = imap_headerinfo($imap, $i, 50, 50);
             $messageDetails[$num++] = [
                 'from' => imap_utf8($header->fromaddress),
@@ -54,7 +62,17 @@ class ImapCommunicator
 
     public function getMailBody($uid)
     {
-        $body = imap_fetchbody($this->imap, $uid, '1', FT_UID);
+        $body = imap_fetchbody($this->imap, imap_msgno($this->imap, $uid), '1');
+        if (empty($body)) {
+            $body = imap_fetchbody($this->imap, imap_msgno($this->imap, $uid), '2');
+        }
         return imap_qprint($body);
+    }
+
+    public function getLastErrorMessage()
+    {
+        if ($this->error instanceof \Throwable) {
+            return $this->error->getMessage();
+        }
     }
 }
